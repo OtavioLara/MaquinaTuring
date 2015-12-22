a95f08aa635358ece4eff1599df1d1c51078b635
Otávio Lara <otavioneveslara@gmail.com>
2015-12-22 17:10:40 -0200

    começo funcao computacao

--- src/maquinaturing/Computacao.java
+++ src/maquinaturing/Computacao.java
@@ -10,5 +10,38 @@ package maquinaturing;
  * @author otavio
  */
 public class Computacao {
-    
+    private MaquinaTuring mt;
+    private int cabeca;
+    Computacao(MaquinaTuring mt,int cabeca){
+        this.mt = mt;
+        this.cabeca = cabeca;
+        this.executa();
+    }
+    Computacao(MaquinaTuring mt){
+        this.mt = mt;
+        this.executa();
+    }
+    Computacao(){}
+    private boolean validaTransicao(int index){
+        if(mt.getEstados().getEstados().contains(mt.getTransicao().get(index).getEstadoCorrente())){
+            if(mt.getEstados().getEstados().contains(mt.getTransicao().get(index).getProximoEstado())){
+                if(mt.getAlfabeto().getAlfabeto().contains(mt.getTransicao().get(index).getSimboloCorrente())){
+                    if(mt.getAlfabeto().getAlfabeto().contains(mt.getTransicao().get(index).getProximoSimbolo())){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    private boolean moverFita(int index){
+        if(this.validaTransicao(index)){
+                return true;
+        }
+    }
+
+    private void executa() {
+        
+        while(e)
+    }
 }
